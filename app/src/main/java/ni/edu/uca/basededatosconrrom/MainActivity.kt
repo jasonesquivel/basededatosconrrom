10cfdf217db656e1207fe00b378e64959d99a2cc
jasonesquivel <95312146+jasonesquivel@users.noreply.github.com>
2022-11-15 20:36:12 -0600

    Binding aplicado

--- app/src/main/java/ni/edu/uca/basededatosconrrom/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/basededatosconrrom/MainActivity.kt
@@ -2,10 +2,13 @@ package ni.edu.uca.basededatosconrrom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ni.edu.uca.basededatosconrrom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
